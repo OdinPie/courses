f8ecd2258138415bfe505981335e587f8c59c00c
Maliha <maliha16tabassum@gmail.com>
2023-09-15 19:01:10 +0600

    added sweet alert

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import { useState } from 'react'
 import './App.css'
 import Cards from './components/Cards/Cards'
 import List from './components/List/List'
+import Swal from 'sweetalert2'
 function App() {
   const [courses, setCourses] = useState([])
   const [totalCredit, setTotalCredit] = useState(0)
   const [remCredit, setRemCredit] = useState(20)
+  const isExist = id=>{
+    const temp =courses.find(course=>course.id===id);
+    console.log(temp);
+  } 
   const handleCourse = (course,credit)=>{
+    isExist(course.id);
     if(totalCredit+credit<=20 && (20-remCredit)>=0){
     const newCourse = [...courses,course]
     setCourses(newCourse);
@@ -16,7 +22,28 @@ function App() {
     setRemCredit(newRemCredit);
     }
     else{
-      alert('GET OUT!');
+      let timerInterval
+Swal.fire({
+  title: 'You can not enroll into this course',
+  // html: 'I will close in <b></b> milliseconds.',
+  timer: 2000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading()
+    const b = Swal.getHtmlContainer().querySelector('b')
+    timerInterval = setInterval(() => {
+      b.textContent = Swal.getTimerLeft()
+    }, 100)
+  },
+  willClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
     }
  
   } 
